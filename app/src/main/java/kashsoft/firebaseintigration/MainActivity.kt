3d573afe0e9c1343548ea316556d7f1485c242b9
hammad <hammadbhat126@gmail.com>
2022-07-24 15:56:19 +0400

    datbase intigration

--- app/src/main/java/kashsoft/firebaseintigration/MainActivity.kt
+++ app/src/main/java/kashsoft/firebaseintigration/MainActivity.kt
@@ -2,10 +2,29 @@ package kashsoft.firebaseintigration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var editTextName: EditText
+    lateinit var buttonSend: Button
+
+    val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+    val reference : DatabaseReference = database.reference.child("Users")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editTextName= findViewById(R.id.editTextName)
+        buttonSend = findViewById(R.id.buttonsend)
+
+        buttonSend.setOnClickListener {
+            val userName : String = editTextName.text.toString()
+            reference.child("userName").setValue(userName)
+        }
+
     }
 }
