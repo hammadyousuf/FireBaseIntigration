28130b4b884882aeaa712198c4489eb23e9dbb0d
hammad <hammadbhat126@gmail.com>
2022-08-02 23:06:09 +0400

    GRADLE INTIGRATION

--- app/src/main/java/kashsoft/firebaseintigration/MainActivity.kt
+++ app/src/main/java/kashsoft/firebaseintigration/MainActivity.kt
@@ -9,35 +9,12 @@ import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var editTextName: EditText
-    lateinit var buttonSend: Button
-    lateinit var textViewName : TextView
 
-    val database : FirebaseDatabase = FirebaseDatabase.getInstance()
-    val reference : DatabaseReference = database.reference.child("Users")
-    val reference2 : DatabaseReference = database.reference
-    override fun onCreate(savedInstanceState: Bundle?) {
+override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        editTextName= findViewById(R.id.editTextName)
-        buttonSend = findViewById(R.id.buttonsend)
-        textViewName = findViewById(R.id.textViewName)
-        reference2.addValueEventListener(object : ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val realName: String = snapshot.child("Users").child("name").value as String
-                textViewName.text =realName
-            }
 
-            override fun onCancelled(p0: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-        })
-
-        buttonSend.setOnClickListener {
-            val userName : String = editTextName.text.toString()
-            reference.child("userName").setValue(userName)
-        }
 
     }
 }
